727bc40be7ce159432f9322edfdad113f364f4f9
haeriee <97428090+haeriee@users.noreply.github.com>
2022-05-12 14:48:57 +0900

    0512

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ for(let i = 0 ; i < images.length; i++) {
     })
 }
 
+
+const saTriggerMargin = 300;
+const saElementList = document.querySelectorAll('.treatment');
+
+const saFunc = function() {
+for (const element of saElementList) {
+    if (!element.classList.contains('show')) {
+    if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+            element.classList.add('show');
+            }
+        }
+    }
+}
+
+window.addEventListener('load', saFunc);
+window.addEventListener('scroll', saFunc);
